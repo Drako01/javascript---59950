0ec343c507c55822e8324b124b30ace108d764ab
Alejandro Di Stefano <addistefano@live.com.ar>
2024-07-10 22:28:07 -0300

    Clase 01

--- js/index.js
+++ js/index.js
@@ -1,6 +1,133 @@
 
-console.log("Hola mundo");
+// console.log("Hola mundo");
 
-let letra = 'a';
+// let letra = 'a';
 
-console.log(letra);
+// console.log(letra);
+
+// Clase 01
+
+// Declaracion de Variables - Reservo espacio en Memoria
+
+// var nombre = "Alejandro";
+// console.log(nombre);
+
+// /**
+//  * 
+//  * 
+//  * 1000
+//  * 
+//  * 
+//  * 
+//  */
+
+// var nombre = "JavasCript";
+// console.log(nombre);
+
+// var nombre = "JavasCript1";
+// console.log(nombre);
+
+// console.log(nombre, nombre, nombre);
+
+// Empezamos a usar let para declarar Variables
+// Variables
+// let nombreDeUsuario = "Alejandro";
+// console.log(nombreDeUsuario);
+
+// let unaLetra = 'a';
+// // Se puede reasignar
+// unaLetra = 'p';
+
+// // let nombre = "CoderHouse";
+
+// //  Hago la Declaracion y la Asignación - Varibles(Se puede reasigar otro valor)
+// let miPrimerString = "Pongo lo que Yo quiera, si quiero usar -  o si quiero usaar 234234234234";
+// console.log(miPrimerString);
+
+// // Hago una Declaración de Variable:
+// let nombreDeLaVariable;
+
+// // Hago la Asignación:
+// nombreDeLaVariable = "Lo que Yo quiera";
+
+// // Se pueden easignar
+// nombreDeLaVariable = "Reasigne el valor";
+// nombreDeLaVariable = "Reasigne el valor otra vez";
+
+// /************* */
+// // CONSTANTES
+// // Declaramos nuestro primer número:
+// // Constantes Primitivas - Constantes se declaran en Mayúscula
+// // Del tipo Constantes (Nunca se le cambia el Valor)
+// // Lo Primitivo es de una sola cosa, o sea, una Letra o un Número, o también un caracter.
+
+// const PI = 3.14;
+// const LETRA = 'A';
+// const ESPACIO = " ";
+// const CARACTER = '@';
+
+// console.log(PI);
+// console.log(LETRA);
+// console.log(ESPACIO);
+// console.log(CARACTER);
+
+// // Constantes No Primitivas
+// // Un String no es una Constante Primitiva
+// const stringConstante = "Este es un String Constante y no me pueden reasigar";
+// // No se pueden easignar
+
+// /// Operaciones con Variables y/o Constantes
+// // Concatenación:
+// let variablesConcatenadas = miPrimerString + ESPACIO + stringConstante;
+
+// console.log(variablesConcatenadas)
+
+// // Concateno dentro de una Función
+// console.log(miPrimerString + ESPACIO + stringConstante);
+// console.log(miPrimerString + ' ' + stringConstante);
+// console.log(miPrimerString, stringConstante);
+
+
+/// Operaciones Matemáticas
+
+// const CUATRO = 4;
+// const CINCO = 5;
+// const DIEZ = 10;
+
+// // Suma
+// let resultado = CUATRO + CINCO;
+// console.log(resultado)
+
+// // Resta
+// resultado = DIEZ - CUATRO;
+// console.log(resultado)
+
+// // Multiplicación
+// resultado = DIEZ * CUATRO;
+// console.log(resultado)
+
+// // División
+// resultado = DIEZ / CINCO;
+// console.log(resultado)
+
+// /// Prompt y Alert
+// const textoPrevioAlPrompt = "Ingrese su Nombre";
+// const textoPosteriorAlPrompt = ", no se olvide de ningun Nombre!";
+// const textoPrevio = "El Nombre Ingresado es:";
+// // El Prompt no admite la COMA para concatenar
+// let nombreCompleto = prompt(textoPrevioAlPrompt + textoPosteriorAlPrompt);
+// console.log(nombreCompleto);
+
+// // El Alert no admite la COMA para concatenar
+// alert(textoPrevio + ESPACIO + nombreCompleto);
+
+
+/// Prompt de Numeros
+// let numeroIngresado = prompt("Ingrese un número: ");
+// // alert("El número ingresado es: " + numeroIngresado);
+// // alert("El número ingresado es: " + numeroIngresado);
+// // alert("El número ingresado es: " + numeroIngresado);
+
+// console.log(numeroIngresado);
+// console.warn(numeroIngresado);
+// console.error(numeroIngresado);
