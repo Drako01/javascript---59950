c68a1c45b73ef48f1d99b4845c83ca250517208e
Alejandro Di Stefano <addistefano@live.com.ar>
2024-08-05 20:45:59 -0300

    Clase 08

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// Clase 08
+// const PI = Math.PI;
+// console.log(PI);
+// console.log(Math.PI);
+
+// const E = Math.E;
+// console.log(E);
+
+// Min y Max
+console.log(Math.min(-1, 5, 9, 125, -500, 1000, -236))
+
+
+const PI = Math.PI;
+const E = Math.E;
+const A = 6.59;
+const B = 5.12;
+const C = 0.49;
+
+// CEIL
