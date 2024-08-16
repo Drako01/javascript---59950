08986b043500a7ae1ba31df29261294b045dc792
Alejandro Di Stefano <addistefano@live.com.ar>
2024-08-15 22:20:57 -0300

    Buscador de productos usando un filtro

--- js/productos.js
+++ js/productos.js
@@ -22,19 +22,27 @@ function addToCart(productoID) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    function renderizarProductos() {
+    function renderizarProductos(productosFiltrados = productos) {
         const productList = document.getElementById('product-list');
         productList.innerHTML = '';
-        productos.forEach(producto => {
+        productosFiltrados.forEach(producto => {
             const productoDiv = document.createElement('div');
             productoDiv.innerHTML = `
-            <p>Nombre: ${producto.nombre} - Precio: $${producto.precio}</p>
-            <button onclick="addToCart(${producto.id})" class="button_grey" >Agregar al Carrito</button>
-        `
+                <p>Nombre: ${producto.nombre} - Precio: $${producto.precio}</p>
+                <button onclick="addToCart(${producto.id})" class="button_grey" >Agregar al Carrito</button>
+            `;
             productList.appendChild(productoDiv);
-        })
-    }   
-    renderizarProductos();
+        });
+    }
 
-    
+    const buscadorInput = document.getElementById('buscador-input');
+    buscadorInput.addEventListener('input', function () {
+        const textoBusqueda = buscadorInput.value.toLowerCase();
+        const productosFiltrados = productos.filter(producto => 
+            producto.nombre.toLowerCase().includes(textoBusqueda)
+        );
+        renderizarProductos(productosFiltrados);
+    });
+
+    renderizarProductos();
 });
