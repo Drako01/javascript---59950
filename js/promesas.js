09c7117851a96b15e18007a121f5054e0c8f094f
Alejandro Di Stefano <addistefano@live.com.ar>
2024-08-28 22:30:48 -0300

    Clase 15

---
+++ js/promesas.js
@@ -0,0 +1,129 @@
+// setTimeout
+
+//setTimeout(funcionAEjecutar, retardoEnMilisegundos);
+
+// console.log("Hola");
+
+// setTimeout(() => {
+//     console.log("Mensaje del medio");
+// }, 0) // 2 Segundos despues
+
+// console.log("Chau");
+
+// setInterval()
+
+// setInterval(()=>{
+//     console.log("Hola gente");
+// }, 2000);
+
+//clearTimeout()
+//clearInterval()
+
+// let contador = 0;
+
+// const interval = setInterval(() => {
+//     contador++;
+//     console.log("Vuelta Nro: " + contador);
+
+//     if (contador === 10) {
+//         clearInterval(interval);
+//         console.log("Proceso terminado.!")
+//     }
+// }, 1000);
+
+
+// console.log("Comenzamos...");
+
+// const finalizar  = setTimeout(() => {
+//     console.log("Proceso finalizado.!")
+// }, 2000);
+
+// clearTimeout(finalizar);
+
+// Promesas
+
+// const pasarDeNivel = (condicionesCumplidas) =>{
+//     return new Promise((resolve, reject) => {        
+//         condicionesCumplidas ?
+//             resolve("Promesa resulta") :        
+//             reject("Promesa Rechazada");   
+//     })
+// };
+
+// console.log(pasarDeNivel(true));
+// console.log(pasarDeNivel(false));
+
+// pasarDeNivel(true)
+//     .then((response) => {
+//         console.log(response);
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
+//     .finally(()=>{
+//         console.log("Ejecuto el Finally de Resulta" );
+//     })
+
+// pasarDeNivel(false)
+//     .then((response) => {
+//         console.log(response);
+//     })
+//     .catch((error) => {
+//         console.error(error)
+//     })
+//     .finally(()=>{
+//         console.log("Ejecuto el Finally de Rechazada");
+//     })
+
+
+// Implementacion
+const productosPromesa = JSON.parse(localStorage.getItem("Cart"));
+
+// [
+//     {id: 1, nombre: "Azucar", precio: 1200},
+//     {id: 2, nombre: "Yerba", precio: 900},
+//     {id: 3, nombre: "Tacos", precio: 1350},
+//     {id: 4, nombre: "Lomito", precio: 1550},
+//     {id: 5, nombre: "Harina", precio: 1600},
+//     {id: 6, nombre: "Fernet", precio: 10500},
+// ]
+
+const mostrarProductos = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(productosPromesa)
+        }, 2000)
+    })
+}
+
+let arrayProductos = [];
+
+const renderizarProductos = (arr) => {
+    const contenedor = document.getElementById("productos-container");
+    contenedor.innerHTML = '';
+    arr.forEach(producto => {
+        const divProducto = document.createElement("div");
+        divProducto.className = 'producto';
+        divProducto.innerHTML = `
+            <h4>ID: ${producto.id}</h4>
+            <h4>Nombre: ${producto.nombre}</h4>
+            <p>Precio: $${producto.precio}.-</p>
+        `;
+        contenedor.appendChild(divProducto);
+    });
+}
+
+
+mostrarProductos()
+    .then((res) =>{
+        arrayProductos = res; // Capturo todo el Array de Productos
+        renderizarProductos(arrayProductos);
+    })
+    .catch((error) => {
+        // console.error("Promesa Rechazada", error);
+        Swal.fire("Promesa Rechazada", "error")
+    })
+    .finally(() => {
+        console.log("Fin del Renderizado");
+    })
+
