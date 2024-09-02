48deb90d03458286260b9fefa2c48e32c287472a
Alejandro Di Stefano <addistefano@live.com.ar>
2024-09-02 16:55:01 -0300

    Clase 16

--- js/navegacion.js
+++ js/navegacion.js
@@ -38,6 +38,10 @@ const enlaces = [
         nombre: "Contacto"
     },
     {
+        link: "promesas",
+        nombre: "Promesas"
+    },
+    {
         link: "login",
         nombre: "Login"
     }
--- js/promesas.js
+++ js/promesas.js
@@ -146,3 +146,5 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 })
+
+
